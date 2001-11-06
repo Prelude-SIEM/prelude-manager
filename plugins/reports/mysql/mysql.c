affec1b98f0db1b7dd3ee8e7c23fbe2710b8a3a5
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2001-11-06 00:54:16 +0000

    *** empty log message ***
    git-svn-id: file:///home/yoann/SVN/trunk/prelude-manager@339 09c5ec92-17d4-0310-903a-819935f44dba

--- plugins/reports/mysql/mysql.c
+++ plugins/reports/mysql/mysql.c
@@ -1,6 +1,6 @@
 /*****
 *
-* Copyright (C) 1998 - 2000 Vandoorselaere Yoann <yoann@mandrakesoft.com>
+* Copyright (C) 2001 Vandoorselaere Yoann <yoann@mandrakesoft.com>
 * Copyright (C) 2001 Sylvain GIL <prelude@tootella.org>
 * All Rights Reserved
 *
@@ -30,27 +30,73 @@
 #include <errno.h>
 
 #include <mysql/mysql.h>
+
 #include "config.h"
 #include "report.h"
 
-/* db stuff */
-/* TODO: get this from prelude-report.conf */
-#define DBHOST "localhost"
-#define DBNAME "prelude"
-#define DBUSER "db"
-#define DBPASS ""
+
 #define MAX_QUERY_LENGTH 8192
 
+
+
+static char *dbhost = NULL;
+static char *dbname = NULL;
+static char *dbuser = NULL;
+static char *dbpass = NULL;
 static MYSQL *connection, mysql;
 
-/* ---------------------- Prototypes ------------------------------------ */
 
-char * db_escape(const char * string);
-int db_insert(char * table, char * fields, char * values);
-static void db_close(void);
-static int db_connect(void);
 
-/* ---------------------------------------------------------------------- */
+/*
+ * Takes a string and create a legal SQL string from it.
+ * returns the escaped string.
+ */
+static char *db_escape(const char *string)
+{
+        char *escaped;
+
+        /*
+         * MySQL documentation say :
+         * The string pointed to by from must be length bytes long. You must
+         * allocate the to buffer to be at least length*2+1 bytes long. (In the
+         * worse case, each character may need to be encoded as using two bytes,
+         * and you need room for the terminating null byte.)
+         */
+        escaped = malloc(strlen(string) * 2 + 1);
+        if (! escaped) {
+                log(LOG_ERR, "memory exhausted.\n");
+                return;
+        }
+
+        mysql_real_escape_string(&mysql, escaped, string, strlen(string));
+
+        return escaped;
+}
+
+
+
+
+/*
+ * insert the given values into the given db table.
+ */
+static int db_insert(char *table, char *fields, char *values)
+{
+        int ret = 0;
+        char insert_query[MAX_QUERY_LENGTH];
+
+        snprintf(insert_query, MAX_QUERY_LENGTH,
+                 "INSERT INTO %s (%s) VALUES(%s)",
+                 table, fields, values);
+
+        if ( (ret = db_query(insert_query)) ) {
+                printf("db_query returned %d\n", ret);
+                ret = -1;
+        }
+        
+        return ret;
+}
+
+
 
 
 static void print_address(idmef_address_t *addr) 
@@ -229,120 +275,127 @@ static void print_alert(idmef_alert_t *alert)
 }
 
 
+
+
 /*
  * plugin callback
  */
 static void db_run(idmef_alert_t *alert)
 {
-        int i;
         print_alert(alert);
 }
 
 
-/*
- * Takes a string and create a legal SQL string from it.
- * returns the escaped string.
- */
-char * db_escape(const char * string)
-{
-        char * escaped;
-
-        escaped = (char *) malloc(strlen(string)*2+1);
-        if (! escaped)
-        {
-                log(LOG_ERR, "memory exhausted.\n");
-                return;
-        }
-
-        mysql_real_escape_string(&mysql, escaped, string, strlen(string));
-
-        return escaped;
-}
-
-
-/*
- * insert the given values into the given db table.
- */
-int db_insert(char * table, char * fields, char * values)
-{
-        char insert_query[MAX_QUERY_LENGTH];
-        int ret = 0;
-
-        snprintf(insert_query, MAX_QUERY_LENGTH,
-                 "INSERT INTO %s (%s) VALUES(%s)",
-                 table, fields, values);
-
-        if ((ret = db_query(insert_query)))
-        {
-                printf("db_query returned %d\n", ret);
-                ret = -1;
-        }
-
-        return ret;
-}
 
 
 /*
  * closes the DB connection.
  */
-static void
-db_close(void)
+static void db_close(void)
 {
         mysql_close(connection);
         printf("mysql connection closed\n");
 }
 
 
+
 /*
  * Connect to the MySQL database
  */
-static int
-db_connect()
+static int db_connect(void)
 {
         int state;
 
-        /* connect to the mySQL database */
-        connection = mysql_connect(&mysql, DBHOST, DBUSER, DBPASS);
-
-        /* check for a connection error */
-        if (connection == NULL)
-        {
-                /* print the error message stored in MsqlErrMsg */
-                printf(mysql_error(&mysql));
-                return (-1);
+        /*
+         * connect to the mySQL database
+         */
+        connection = mysql_connect(&mysql, dbhost, dbuser, dbpass);
+        if ( ! connection ) {
+                log(LOG_INFO, "%s", mysql_error(&mysql));
+                return -1;
         }
 
-        /* select which database to use on the server */
-        state = mysql_select_db(connection, DBNAME);
+        /*
+         * select which database to use on the server
+         */
+        state = mysql_select_db(connection, dbname);
 
         /* -1 means an error occurred */
-        if (state == -1)
-        {
-                printf(mysql_error(connection));
-
-                /* close up our connection before exiting */
+        if (state == -1) {
+                log(LOG_INFO, "%s", mysql_error(connection));
                 mysql_close(connection);
-                return (-1);
+                return -1;
         }
 
-        return 1;
+        return 0;
 }
+
+
+
+static void set_dbhost(const char *optarg) 
+{
+        dbhost = strdup(optarg);
+}
+
+
+
+static void set_dbname(const char *optarg) 
+{
+        dbname = strdup(optarg);
+}
+
+
+static void set_dbuser(const char *optarg) 
+{
+        dbuser = strdup(optarg);
+}
+
+
+static void set_dbpass(const char *optarg) 
+{
+        dbpass = strdup(optarg);
+}
+
+
+
+static void print_help(const char *optarg) 
+{
+        fprintf(stderr, "Usage for MySQL :\n");
+        fprintf(stderr, "-d --dbhost Tell the host where the MySQL DB is located.\n");
+        fprintf(stderr, "-n --dbname Tell the name of the database to use.\n");
+        fprintf(stderr, "-u --dbuser Username to use for database login.\n");
+        fprintf(stderr, "-p --dbpass Password to use for database login.\n");
+}
+
+
 
 
 int plugin_init(unsigned int id)
 {
         int ret;
         static plugin_report_t plugin;
-        
+        plugin_option_t opts[] = {
+                { "dbhost", required_argument, NULL, 'd', set_dbhost },
+                { "dbname", required_argument, NULL, 'n', set_dbname },
+                { "dbuser", required_argument, NULL, 'u', set_dbuser },
+                { "dbpass", required_argument, NULL, 'p', set_dbpass },
+                { "help", no_argument, NULL, 'h', print_help         },
+                { 0, 0, 0, 0 },
+        };
+
         plugin_set_name(&plugin, "MySQL");
         plugin_set_desc(&plugin, "Will log all alert to a MySQL database.");
         plugin_set_running_func(&plugin, db_run);
         plugin_set_closing_func(&plugin, db_close);
-
+        
+        plugin_config_get((plugin_generic_t *)&plugin, opts, PRELUDE_REPORT_CONF);
+        
+        if ( !dbhost || !dbname || !dbuser || !dbpass )
+                return -1;
+        
         /* connect to db or exit */
         ret = db_connect();
-        if (!ret)
-        {
+        if ( ! ret ) {
                 printf("db_connect returned %d\n", ret);
                 return -1;
         }
