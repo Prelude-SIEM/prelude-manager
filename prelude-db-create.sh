4bef2e1e43ba3f7d74188ef3078f986809b37997
Laurent Oudot <oudot.laurent@wanadoo.fr>
2002-03-31 23:50:57 +0000

    2002-04-01  Laurent Oudot <oudot.laurent@wanadoo.fr>
    	* prelude-db-create.sh:
    	Added postgresql support for the IDMEF database
    
    
    git-svn-id: file:///home/yoann/SVN/trunk/prelude-manager@930 09c5ec92-17d4-0310-903a-819935f44dba

--- prelude-db-create.sh
+++ prelude-db-create.sh
@@ -23,11 +23,6 @@
 #####
 
 # This is a script that aims at creating needed databases by the prelude project during the installation process.
-# At this time this is just a test version that moreover only work for mysql...
-# It will contain at least two parts :
-# 1) Creation of IDMEFv6 Database
-# 2) Creation of the Frontend Database (used in prelude-php-frontend)
-
 
 ####
 # Prelude IDMEF6 Database part
@@ -36,29 +31,99 @@ db_user="root"
 db_password=""
 db_name="prelude"
 
+db_type="" # Type can be either mysql or pgsql
+
 manager_password=""
 manager_user="prelude"
 
+warning () {
+	answer=""
+	clear
+	echo
+	echo "Prelude Database Support Installation"
+	echo "====================================="
+	echo
+	echo "*** Phase 0/5 ***"
+	echo
+	echo "Warning: if you want to use database support with prelude"
+	echo " You should dedicate the database for this job only."
+	echo
+	echo "So if you ever have a database running for another job"
+	echo " please think about taking it away, because this script"
+	echo " will install prelude as a dedicated database and you"
+	echo " could meet some troubles with your old bases."
+	echo
+	echo "Do you want to install a dedicated database for prelude ?"
+ 	echo -n " (y)es / (n)o : "
+	read -n 1 answer
+	case $answer in
+	    y)
+		echo
+		;;
+	    n)
+	        echo
+		echo "Installation aborted on user choice."
+		echo
+		exit
+		;;
+	    *)
+		echo
+		echo "Please read the warning message and choose y or n"
+		read
+		warning
+	esac
 
+}
+
+ask_db_type () {
+	answer=""
+	echo
+	echo "*** Phase 1/5 ***"
+	echo
+	echo -n "Enter the type of the database [mysql|pgsql]: "
+	read -n 5 answer
+	
+	if [[ "${answer}" == "mysql" || "${answer}" == "pgsql" ]]; then
+		db_type=${answer}
+	else
+		ask_db_type
+	fi
+}
 
 ask_db_name () {
+	answer=""
+	echo
+	echo
+	echo "*** Phase 2/5 ***"
 	echo
 	echo -n "Enter the name of the Prelude database [${db_name}]: "
 	read answer
 	
 	if [[ "${answer}" != "" && "${answer}" != "${db_name}" ]]; then
-		db_name=${answer};
+		db_name=${answer}
 	fi
 }
 
 
 ask_db_user () {
+	answer=""
+	case $db_type in
+	    mysql)
+		db_user="root"
+		;;
+	    pgsql)
+		db_user="postgres"
+		;;
+	esac
+
+	echo
+	echo "*** Phase 3/5 ***"
 	echo
 	echo -n "What is the database administrative user ? [${db_user}]: "
 	read answer
 
 	if [[ "${answer}" != "" && "${answer}" != "${db_user}" ]]; then
-		db_user = ${answer};
+		db_user=${answer}
 	fi
 
 }
@@ -66,6 +131,8 @@ ask_db_user () {
 
 
 ask_manager_user() {
+	answer=""
+	echo "*** Phase 4/5 ***"
 	echo
 	echo "We need to create an user to be used by the Prelude Manager in order"
 	echo "to access the \"${db_name}\" database."
@@ -75,13 +142,14 @@ ask_manager_user() {
 	read answer
 
 	if [[ "${answer}" != "" && "${answer}" != "${manager_user}" ]]; then
-		manager_user = ${answer};
+		manager_user=${answer}
 	fi
 }
 
 
 
 ask_password () {
+	answer=""
         echo -n "Please enter a password: "
 	read -s answer
 	echo
@@ -93,7 +161,7 @@ ask_password () {
 	if [[ "${answer}" != "${answerbis}" ]]; then
 	        echo
 		echo "Password mismatch. Please try again"
-		ask_password;
+		ask_password
         else
                 password=${answer}
 	fi
@@ -104,6 +172,8 @@ ask_password () {
 db_confirm ()
 {
 	answer=""
+	echo
+	echo "*** Phase 5/5 ***"
 	echo
 	echo "Please confirm those information before processing" :
 	echo
@@ -140,45 +210,117 @@ db_confirm ()
 db_create ()
 {
 	echo
-	echo "Creating the database..."
-	mysqladmin -u "${db_user}" --password="${db_password}" create "${db_name}"
+	echo "Creating the database ${db_name}..."
+	case $db_type in
+		mysql)
+		    mysqladmin -u "${db_user}" --password="${db_password}" create "${db_name}"
+		    ;;
+		pgsql)
+		    /etc/init.d/postgresql reload
+		    su - "${db_user}" -c createdb "${db_name}"
+		    ;;
+		*)
+		    echo "Weird error with the database type ${db_type}"
+		    exit
+		    ;;
+	esac
 }
 
 
 
 db_create_users ()
 {
-        echo
-        echo "Creating user \"${manager_user}\" for database \"${db_name}\", using \"${db_user}\" to connect to the database.";
-        mysql -u ${db_user} --password=${db_password} -e "GRANT ALL PRIVILEGES ON ${db_name}.* TO ${manager_user}@localhost IDENTIFIED BY '${manager_password}';" ${db_name}
-
 	echo
-        echo "Creating tables with ./plugins/db/mysql/mysql.sql"
-        mysql -u ${manager_user} --password=${manager_password} --database ${db_name} < ./plugins/db/mysql/mysql.sql
+       	echo "Creating user \"${manager_user}\" for database \"${db_name}\", using \"${db_user}\" to connect to the database.";
 
-        echo
-        echo "If everything succeed, you should now be able to launch prelude-manager with thoses parameters :"
-        echo "prelude-manager --mysql --dbhost localhost --dbname ${db_name} --dbuser ${manager_user} --dbpass \"not shown\""
+	case $db_type in
+
+		mysql)
+			mysql -u ${db_user} --password=${db_password} -e "GRANT ALL PRIVILEGES ON ${db_name}.* TO ${manager_user}@localhost IDENTIFIED BY '${manager_password}';" ${db_name}
+
+			echo
+			echo "Creating tables with ./plugins/db/mysql/mysql.sql"
+			mysql -u ${manager_user} --password=${manager_password} --database ${db_name} < ./plugins/db/mysql/mysql.sql
+
+			;;
+		pgsql)
+			(grep prelude-ids_frontend /etc/postgresql/pg_hba.conf ; echo local all ident postgres ) > /etc/postgresql/pg_hba.conf_temp_prelude
+			mv -f /etc/postgresql/pg_hba.conf_temp_prelude /etc/postgresql/pg_hba.conf
+			(echo postgres root postgres; echo postgres root prelude) > /etc/postgresql/pg_ident.conf
+			rm -rf /var/lib/postgres/data/prelude-ids_manager
+			touch /var/lib/postgres/data/prelude-ids_manager
+			(echo "${manager_user}"; echo "${manager_password}"; echo "${manager_password}") | /usr/lib/postgresql/bin/pg_passwd /var/lib/postgres/data/prelude-ids_manager 1>/dev/null
+			/etc/init.d/postgresql reload
+
+			psql -U ${db_user} -d template1 -c "CREATE USER ${manager_user} WITH ENCRYPTED PASSWORD '${manager_password}' NOCREATEDB NOCREATEUSER;"
+			echo
+			echo "Creating tables with ./plugins/db/pgsql/postgres.sql"
+			psql -d "${db_name}" -U "${manager_user}" < ./plugins/db/pgsql/postgres.sql
+
+			(grep prelude-ids_frontend /etc/postgresql/pg_hba.conf ; echo local ${db_name} password prelude-ids_manager ; echo host ${db_name} 127.0.0.1 255.0.0.0 password prelude-ids_manager ; echo local template1 ident postgres ) > /etc/postgresql/pg_hba.conf_temp_prelude
+			mv -f /etc/postgresql/pg_hba.conf_temp_prelude /etc/postgresql/pg_hba.conf
+			rm -rf /etc/postgresql/pg_ident.conf
+			/etc/init.d/postgresql reload
+			;;
+		*)
+			echo "Weird error with the database type ${db_type}"
+			exit
+			;;
+		esac
+       	echo   
+	echo "If everything succeed, you should now be able to launch prelude-manager with thoses parameters :"
+	echo "prelude-manager --${db_type} --dbhost localhost --dbname ${db_name} --dbuser ${manager_user} --dbpass \"not shown\""
 	echo
-	echo "Or you may modify the prelude-manager configuration file to include theses informations"
+	echo "Or you may modify the prelude-manager configuration file to include theses informations :"
+
+	echo "cut here --->"
+	case $db_type in
+		mysql)
+		    echo "[MySQL]"
+		    ;;
+		pgsql)
+		    echo "[PgSQL]"
+		    ;;
+		*)
+		    exit
+		    ;;
+	esac
+	echo "# Host the database is listening on."
+        echo "dbhost = localhost;"
+	echo "# Name of the database."
+	echo "dbname = ${db_name};"
+	echo "# Username to be used to connect the database."
+	echo "dbuser = ${manager_user};"
+	echo "# Password used to connect the database."
+	echo "dbpass = xxxxxx;"
+	echo "<--- cut here"
+	echo 
+	echo "Replace xxxxxx by the password you choose for the manager account"
+	echo "And fill those lines in /usr/local/etc/prelude-manager/prelude-manager.conf"
+	echo
 }
 
 
 
 idmef () {
+	warning
+	ask_db_type
 	ask_db_name
 	ask_db_user
 	
 	echo
-        echo "We need the password of the admin user \"${db_user}\" to log on the database."
-	ask_password
-        db_password=${password}
+
+	if [ "${db_type}" == "mysql" ]; then
+	    echo "We need the password of the admin user \"${db_user}\" to log on the database."
+	    ask_password
+	    db_password=${password}
+	fi
 
 	ask_manager_user
 	echo
 	echo "We need a password for the \"${manager_user}\" account."
 	ask_password
-	manager_password=${password};
+	manager_password=${password}
 	
         db_confirm
 	db_create
@@ -191,8 +333,4 @@ idmef
 # End of the IDMEF6 Database part
 ####
 
-####
-# Frontend Database part
 
-# End of the Frontend Database part
-####
