2c5adb7d9b7dc750ede309f2d62e85b92faded3c
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2002-05-13 17:11:52 +0000

    *** empty log message ***
    git-svn-id: file:///home/yoann/SVN/trunk/prelude-manager@1168 09c5ec92-17d4-0310-903a-819935f44dba

--- src/server-logic.c
+++ src/server-logic.c
@@ -246,14 +246,16 @@ static int handle_fd_event(server_fd_set_t *set, int cnx_key)
                 dprint("thread=%ld - Data available (ret=%d)\n", pthread_self(), ret);       
                 if ( ret < 0 )
                         remove_connection(set, cnx_key);
+                return 0;
         }
         
         else if ( set->pfd[cnx_key].revents & (POLLERR|POLLHUP|POLLNVAL) ) {
                 dprint("thread=%ld - Hanging up.\n", pthread_self());
                 remove_connection(set, cnx_key);
+                return 0;
         }
 
-        return 0;
+        return -1;
 }
 
 
