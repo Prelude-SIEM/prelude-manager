76fc6c597d7f007fb1cba675d7f919f511f007bb
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-05-05 09:19:57 +0000

    * dev/prelude/svn/trunk/plugins-instances/prelude-manager/plugins/filters/filter/filter.c   (parse_plugin_name): implemented.   (set_filter_hook): sscanf sucks - use our own parsing function.
    git-svn-id: file:///home/yoann/SVN/trunk/prelude-manager@3569 09c5ec92-17d4-0310-903a-819935f44dba

--- plugins/filters/filter/filter.c
+++ plugins/filters/filter/filter.c
@@ -1,6 +1,6 @@
 /*****
 *
-* Copyright (C) 2002 Yoann Vandoorselaere <yoann@mandrakesoft.com>
+* Copyright (C) 2002-2004 Yoann Vandoorselaere <yoann@mandrakesoft.com>
 * Copyright (C) 2003 Krzysztof Zaraska  <kzaraska@student.uci.agh.edu.pl>
 * All Rights Reserved
 *
@@ -25,91 +25,124 @@
 /* Adapted Yoann's reference filtering plugin code (skeleton.c) */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 #include <assert.h>
 
 #include <libprelude/list.h>
 #include <libprelude/idmef.h>
-#include <libprelude/plugin-common.h>
-#include <libprelude/plugin-common-prv.h>
-
-#include "report.h"
+#include <libprelude/prelude-log.h>
+#include <libprelude/idmef-criteria.h>
 
 #include "plugin-filter.h"
 
 
-static plugin_filter_t plugin;
-
-static int tbl_index = 0;
-static filter_entry_t tbl[256];
-static idmef_criterion_t *filter_rule = NULL;
+static plugin_filter_t filter_plugin;
 
 
+typedef struct {
+        void *data;
+        idmef_criteria_t *criteria;
+} filter_plugin_t;
 
-static int process_message(const idmef_message_t *msg, void *priv) 
+
+
+static int process_message(idmef_message_t *msg, void *priv) 
 {
 	int ret;
-	
-	ret = idmef_criterion_match(msg, (idmef_criterion_t *) priv);
+	filter_plugin_t *plugin = priv;
+        
+        if ( ! plugin->criteria )
+                return 0;
+        
+	ret = idmef_criteria_match(plugin->criteria, msg);
 	if ( ret < 0 ) {
 		log(LOG_ERR, "criteria matching error: assuming positive match\n");
 		ret = 1;
 	}
 
-        return ( ret > 0 ) ? 0 : -1;
+        return (ret > 0) ? 0 : -1;
 }
 
 
 
-static void add_category(plugin_generic_t *filtered_plugin, filter_category_t cat) 
+
+static int parse_plugin_name(const char *buf, char **pname, char **iname)
 {
-        assert(tbl_index + 1 < sizeof(tbl) / sizeof(filter_entry_t));
+        char *s, *e;
         
-        tbl[tbl_index].category = cat;
-        tbl[tbl_index].plugin = filtered_plugin;
-        tbl[tbl_index].private_data = filter_rule;
-        tbl[++tbl_index].category = FILTER_CATEGORY_END;
+        s = strchr(buf, '[');
+        if ( ! s ) {
+                *pname = strdup(buf);
+                if ( ! *pname ) {
+                        log(LOG_ERR, "memory exhausted.\n");
+                        return -1;
+                }
+
+                return 0;
+        }
         
-        filter_rule = NULL;
+        e = strchr(buf, ']');
+        if ( ! e ) {
+                *s = '[';
+                log(LOG_ERR, "invalid plugin instance name: %s.\n", buf);
+                return -1;
+        }
+        
+        *s = *e = 0;
+        
+        *pname = strdup(buf);
+        *iname = strdup(s + 1);
+        
+        *s = '[';
+        *e = ']';
+        
+        return 0;
 }
 
 
 
-static int set_filter_hook(prelude_option_t *opt, const char *arg) 
+
+static int set_filter_hook(void **context, prelude_option_t *opt, const char *arg) 
 {
         int i, ret;
-        plugin_generic_t *ptr;
+        char *pname, *iname;
+        filter_plugin_t *plugin;
+        prelude_plugin_instance_t *ptr;
         struct {
                 const char *hook;
                 filter_category_t cat;
         } tbl[] = {
-                { "database", FILTER_CATEGORY_DATABASE   },
                 { "reporting", FILTER_CATEGORY_REPORTING },
                 { "relaying", FILTER_CATEGORY_RELAYING   },
                 { NULL, 0                                },
         };
 
+        plugin = prelude_plugin_instance_get_data(*context);
+        
         for ( i = 0; tbl[i].hook != NULL; i++ ) {
                 ret = strcasecmp(arg, tbl[i].hook);
                 if ( ret == 0 ) {
-                        add_category(NULL, tbl[i].cat);
+                        filter_plugins_add_category(*context, tbl[i].cat, plugin);
                         return prelude_option_success;
                 }
         }
 
-        /*
-         * not a known category, the user may want to filter a
-         * specific plugin.
-         */
-
-        ptr = plugin_search_by_name(arg);
+        ret = parse_plugin_name(arg, &pname, &iname);
+        if ( ret < 0 )
+                return -1;
+        
+        ptr = prelude_plugin_search_instance_by_name(pname, iname);
         if ( ! ptr ) {
                 log(LOG_ERR, "category '%s' doesn't exist, or a plugin of that name is not loaded.\n", arg);
                 return prelude_option_error;
         }
 
-        add_category(ptr, FILTER_CATEGORY_PLUGIN);
+        free(pname);
+        free(iname);
+        
+        filter_plugins_add_plugin(*context, ptr, plugin);
         
         return prelude_option_success;
 }
@@ -117,52 +150,77 @@ static int set_filter_hook(prelude_option_t *opt, const char *arg)
 
 
 
-static int set_filter_rule(prelude_option_t *opt, const char *arg) 
+static int set_filter_rule(void **context, prelude_option_t *opt, const char *arg) 
 {
-        filter_rule = idmef_criterion_new_string(arg);
-        return filter_rule ? prelude_option_success : prelude_option_error ;
-}
+        idmef_criteria_t *new;
+        filter_plugin_t *plugin = prelude_plugin_instance_get_data(*context);
 
-
-
-
-static int enable_filter(prelude_option_t *opt, const char *arg) 
-{
-        if ( ! tbl_index )
-                return prelude_option_success;
-
-        filter_rule = NULL;
-        plugin.category = tbl;
-        tbl_index = 0;
+        new = idmef_criteria_new_string(arg);
+        if ( ! new )
+                return prelude_option_error;
         
-        return plugin_subscribe((plugin_generic_t *) &plugin);
+        if ( ! plugin->criteria ) {
+                plugin->criteria = idmef_criteria_new();
+                if ( ! plugin->criteria ) {
+                        idmef_criteria_destroy(new);
+                        return prelude_option_error;
+                }
+        }
+
+        idmef_criteria_add_criteria(plugin->criteria, new, operator_or);
+        
+        return prelude_option_success;
 }
 
 
 
 
-plugin_generic_t *plugin_init(int argc, char **argv)
+static int filter_activate(void **context, prelude_option_t *opt, const char *arg) 
+{
+        filter_plugin_t *new;
+
+        new = malloc(sizeof(*new));
+        if ( ! new ) {
+                log(LOG_ERR, "memory exhausted.\n");
+                return prelude_option_error;
+        }
+        
+        new->criteria = NULL;
+
+        *context = prelude_plugin_subscribe((void *) &filter_plugin, opt, arg, new);
+        if ( ! *context ) {
+                free(new);
+                return prelude_option_error;
+        }
+        
+        return prelude_option_success;
+}
+
+
+
+
+prelude_plugin_generic_t *prelude_plugin_init(void)
 {
         prelude_option_t *opt;
         
         opt = prelude_option_add(NULL, CLI_HOOK|CFG_HOOK|WIDE_HOOK, 0, "filter",
-                                 "Option for the filter plugin", no_argument,
-                                 enable_filter, NULL);
+                                 "Option for the filter plugin", optionnal_argument,
+                                 filter_activate, NULL);
 
         prelude_option_add(opt, CLI_HOOK|CFG_HOOK|WIDE_HOOK, 'r', "rule",
                                  "Filtering rule", required_argument,
                                  set_filter_rule, NULL);
         
         prelude_option_add(opt, CLI_HOOK|CFG_HOOK|WIDE_HOOK, 'h', "hook",
-                           "Where the filter should be hooked (reporting|database|relaying|plugin name)",
+                           "Where the filter should be hooked (reporting|relaying|plugin name)",
                            required_argument, set_filter_hook, NULL);
         
-        plugin_set_name(&plugin, "Filter");
-        plugin_set_author(&plugin, "Krzysztof Zaraska");
-        plugin_set_contact(&plugin, "kzaraska@student.uci.agh.edu.pl");
-        plugin_set_desc(&plugin, "Match alert against IDMEF criteria");
-	plugin_set_running_func(&plugin, process_message);
+        prelude_plugin_set_name(&filter_plugin, "Filter");
+        prelude_plugin_set_author(&filter_plugin, "Krzysztof Zaraska");
+        prelude_plugin_set_contact(&filter_plugin, "kzaraska@student.uci.agh.edu.pl");
+        prelude_plugin_set_desc(&filter_plugin, "Match alert against IDMEF criteria");
+	prelude_plugin_set_running_func(&filter_plugin, process_message);
                 
-	return (plugin_generic_t *) &plugin;
+	return (void *) &filter_plugin;
 }
 
