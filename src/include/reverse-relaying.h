3d9c28db125c3a456e488d84d246eb839f3832d8
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-06-25 01:50:40 +0000

    fix prototype.
    git-svn-id: file:///home/yoann/SVN/trunk/prelude-manager@3823 09c5ec92-17d4-0310-903a-819935f44dba

--- src/include/reverse-relaying.h
+++ src/include/reverse-relaying.h
@@ -4,7 +4,7 @@ int reverse_relay_tell_dead(prelude_connection_t *cnx);
 
 int reverse_relay_add_receiver(prelude_connection_t *cnx);
 
-prelude_client_t *reverse_relay_search_receiver(const char *addr);
+prelude_connection_t *reverse_relay_search_receiver(const char *addr);
 
 void reverse_relay_send_msg(idmef_message_t *idmef);
 
