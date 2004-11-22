f6c91e6bb157a674c7ab1979dcc6dfcfaa065048
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2004-11-22 12:34:04 +0000

    Add missing copyright notice. Remove report-infos.[ch] which are deprecated for a long time now.
    git-svn-id: file:///home/yoann/SVN/trunk/prelude-manager@4416 09c5ec92-17d4-0310-903a-819935f44dba

--- src/include/reverse-relaying.h
+++ src/include/reverse-relaying.h
@@ -1,3 +1,26 @@
+/*****
+*
+* Copyright (C) 2004 Yoann Vandoorselaere <yoann@prelude-ids.org>
+* All Rights Reserved
+*
+* This file is part of the Prelude program.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*****/
+
 int reverse_relay_tell_receiver_alive(prelude_connection_t *cnx);
 
 int reverse_relay_tell_dead(prelude_connection_t *cnx);
