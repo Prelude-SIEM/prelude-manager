3ac936eb99665c35b0f9c1e244add7c265b1a4ef
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2007-06-15 16:52:44 +0000

    Whitespace police.
    git-svn-id: file:///home/yoann/SVN/prelude-manager/trunk@9647 09c5ec92-17d4-0310-903a-819935f44dba

--- src/prelude-manager.c
+++ src/prelude-manager.c
@@ -6,7 +6,7 @@
 * This file is part of the Prelude-Manager program.
 *
 * This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by 
+* it under the terms of the GNU General Public License as published by
 * the Free Software Foundation; either version 2, or (at your option)
 * any later version.
 *
@@ -65,10 +65,10 @@ static volatile sig_atomic_t got_signal = 0;
 /*
  * all function called here should be signal safe.
  */
-static RETSIGTYPE handle_signal(int sig) 
+static RETSIGTYPE handle_signal(int sig)
 {
         size_t i;
-        
+
         /*
          * stop the sensor server.
          */
@@ -80,14 +80,14 @@ static RETSIGTYPE handle_signal(int sig)
 
 
 
-static void restart_manager(void) 
+static void restart_manager(void)
 {
         int ret;
-        
+
         prelude_log(PRELUDE_LOG_WARN, "- Restarting Prelude Manager (%s).\n", global_argv[0]);
-        
+
         ret = execvp(global_argv[0], global_argv);
-        if ( ret < 0 ) 
+        if ( ret < 0 )
                 prelude_log(LOG_ERR, "Error restarting Prelude Manager (%s).\n", global_argv[0]);
 }
 
@@ -106,7 +106,7 @@ static int fill_analyzer_infos(void)
         if ( ret < 0 )
                 return ret;
         idmef_analyzer_set_version(local, str);
-        
+
         ret = prelude_string_new_constant(&str, MANAGER_MODEL);
         if ( ret < 0 )
                 return ret;
@@ -139,15 +139,15 @@ int main(int argc, char **argv)
         int ret;
         struct sigaction action;
         prelude_option_t *manager_root_optlist;
-        
+
         prelude_init(&argc, argv);
-        
+
         global_argv = argv;
         prelude_option_new_root(&manager_root_optlist);
-        
+
         /*
          * make sure we ignore sighup until acceptable.
-         */        
+         */
 #ifdef SA_INTERRUPT
         action.sa_flags = SA_INTERRUPT;
 #else
@@ -156,7 +156,7 @@ int main(int argc, char **argv)
         action.sa_handler = SIG_IGN;
         sigemptyset(&action.sa_mask);
         sigaction(SIGHUP, &action, NULL);
-        
+
         /*
          * Initialize plugin first.
          */
@@ -182,23 +182,23 @@ int main(int argc, char **argv)
                 return -1;
         }
         prelude_log(PRELUDE_LOG_DEBUG, "- Initialized %d filtering plugins.\n", ret);
-        
-        
+
+
         ret = manager_options_init(manager_root_optlist, &argc, argv);
         if ( ret < 0 )
                 return -1;
 
         ret = prelude_client_new(&manager_client, DEFAULT_ANALYZER_NAME);
         if ( ret < 0 ) {
-                prelude_perror(ret, "error creating prelude-client object");                
+                prelude_perror(ret, "error creating prelude-client object");
                 return -1;
         }
-        
+
         fill_analyzer_infos();
         prelude_client_set_heartbeat_cb(manager_client, heartbeat_cb);
         prelude_client_set_flags(manager_client, prelude_client_get_flags(manager_client) & ~PRELUDE_CLIENT_FLAGS_CONNECT);
         prelude_client_set_config_filename(manager_client, config.config_file);
-        
+
         ret = prelude_client_init(manager_client);
         if ( ret < 0 ) {
                 prelude_perror(ret, "error initializing prelude-client");
@@ -208,10 +208,10 @@ int main(int argc, char **argv)
         ret = manager_options_read(manager_root_optlist, &argc, argv);
         if ( ret < 0 )
                 return -1;
-        
+
         ret = prelude_client_start(manager_client);
         if ( ret < 0 ) {
-                prelude_perror(ret, "error starting prelude-client");                
+                prelude_perror(ret, "error starting prelude-client");
                 return -1;
         }
 
@@ -229,44 +229,44 @@ int main(int argc, char **argv)
                 prelude_log(PRELUDE_LOG_ERR, "couldn't initialize alert scheduler.\n");
                 return -1;
         }
-        
+
         /*
          * start server
          */
-        ret = manager_auth_init(manager_client, config.dh_bits, config.dh_regenerate);        
-	if ( ret < 0 ) {
+        ret = manager_auth_init(manager_client, config.dh_bits, config.dh_regenerate);
+        if ( ret < 0 ) {
                 prelude_log(PRELUDE_LOG_WARN, "%s\n", prelude_client_get_setup_error(manager_client));
                 return -1;
         }
-        
+
         /*
          * setup signal handling
          */
         sigaction(SIGPIPE, &action, NULL);
-        
+
         action.sa_handler = handle_signal;
         sigaction(SIGINT, &action, NULL);
         sigaction(SIGTERM, &action, NULL);
         sigaction(SIGABRT, &action, NULL);
         sigaction(SIGQUIT, &action, NULL);
         sigaction(SIGHUP, &action, NULL);
-        
+
         server_generic_start(config.server, config.nserver);
-                
+
         /*
          * we won't get there unless a signal is caught.
          */
         if ( got_signal )
                 prelude_log(PRELUDE_LOG_WARN, "signal %d received, %s prelude-manager.\n",
                             got_signal, (got_signal == SIGHUP) ? "will restart" : "terminating");
-        
+
         idmef_message_scheduler_exit();
 
         if ( got_signal == SIGHUP )
                 restart_manager();
-        
+
         if ( config.pidfile )
                 unlink(config.pidfile);
-        
-	exit(0);	
+
+        exit(0);
 }
