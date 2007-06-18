14cc214459649708bdd4162888077aab79b3928a
Yoann Vandoorselaere <yoann.v@prelude-ids.com>
2007-06-18 07:38:44 +0000

    Improve IPv4 / IPv6 address normalization.
    IPv4 mapped IPv6 addresses are now mapped back to IPv4.
    Additionally, the Normalize plugin now provide two additionals option:
    
    - ipv6-only: Map any incoming IPv4 address to IPv6.
    - keep-ipv4-mapped-ipv6: do not map IPv4 mapped IPv6 addresses back to IPv4.
    
    refs #232.
    
    
    git-svn-id: file:///home/yoann/SVN/prelude-manager/trunk@9659 09c5ec92-17d4-0310-903a-819935f44dba

--- plugins/decodes/normalize/normalize.c
+++ plugins/decodes/normalize/normalize.c
@@ -37,6 +37,11 @@ int normalize_LTX_manager_plugin_init(prelude_plugin_entry_t *pe, void *root_opt
 
 
 
+static prelude_bool_t no_ipv6_prefix = TRUE;
+static prelude_bool_t normalize_to_ipv6 = FALSE;
+
+
+
 static int sanitize_service_protocol(idmef_service_t *service)
 {
         int ret;
@@ -46,8 +51,6 @@ static int sanitize_service_protocol(idmef_service_t *service)
 
         if ( ! service )
                 return 0;
-
-        setprotoent(1);
 
         ipn = idmef_service_get_iana_protocol_number(service);
         if ( ipn ) {
@@ -84,22 +87,61 @@ static int sanitize_service_protocol(idmef_service_t *service)
 
 
 
+
+static void sanitize_address_string(idmef_address_t *addr, const char *str, prelude_bool_t have_v6_prefix)
+{
+        int ret;
+        prelude_string_t *pstr;
+
+        if ( have_v6_prefix && no_ipv6_prefix && ! normalize_to_ipv6 ) {
+                ret = prelude_string_new_dup(&pstr, str + 7);
+                if ( ret < 0 )
+                        return;
+
+                idmef_address_set_address(addr, pstr);
+        }
+
+        else if ( ! have_v6_prefix && normalize_to_ipv6 ) {
+                ret = prelude_string_new_dup(&pstr, "::ffff:");
+                if ( ret < 0 )
+                        return;
+
+                ret = prelude_string_cat(pstr, str);
+                if ( ret < 0 ) {
+                        prelude_string_destroy(pstr);
+                        return;
+                }
+
+                idmef_address_set_address(addr, pstr);
+                idmef_address_set_category(addr, IDMEF_ADDRESS_CATEGORY_IPV6_ADDR);
+        }
+
+        return;
+}
+
+
 static void sanitize_address(idmef_address_t *addr)
 {
         int ret;
         const char *str;
         int a, b, c, d;
         char buf1[256], buf2[256];
+        prelude_bool_t ipv6_prefix = FALSE;
 
-        if ( idmef_address_get_category(addr) != IDMEF_ADDRESS_CATEGORY_UNKNOWN )
+        if ( idmef_address_get_category(addr) != IDMEF_ADDRESS_CATEGORY_UNKNOWN ||
+             ! idmef_address_get_address(addr) )
                 return;
+
 
         str = prelude_string_get_string(idmef_address_get_address(addr));
 
-        ret = sscanf(str, "%d.%d.%d.%d", &a, &b, &c, &d);
+        if ( strncmp(str, "::ffff:", 7) == 0 )
+                ipv6_prefix = TRUE;
+
+        ret = sscanf(str + ((ipv6_prefix) ? 7 : 0), "%d.%d.%d.%d", &a, &b, &c, &d);
         if ( ret == 4 ) {
                 idmef_address_set_category(addr, IDMEF_ADDRESS_CATEGORY_IPV4_ADDR);
-                return;
+                return sanitize_address_string(addr, str, ipv6_prefix);
         }
 
         ret = sscanf(str, "%255[^@]@%255s", buf1, buf2);
@@ -119,11 +161,18 @@ static void sanitize_address(idmef_address_t *addr)
 static int sanitize_node(idmef_node_t *node)
 {
         const char *str;
+        prelude_string_t *pstr;
         idmef_address_t *address = NULL;
 
         while ( (address = idmef_node_get_next_address(node, address)) ) {
 
-                str = prelude_string_get_string(idmef_address_get_address(address));
+                pstr = idmef_address_get_address(address);
+                if ( ! pstr ) {
+                        idmef_address_destroy(address); address = NULL;
+                        continue;
+                }
+
+                str = prelude_string_get_string(pstr);
                 if ( ! str || ! *str ) {
                         idmef_address_destroy(address); address = NULL;
                         continue;
@@ -222,16 +271,45 @@ static int normalize_run(prelude_msg_t *msg, idmef_message_t *idmef)
 
 
 
+static int normalize_to_ipv6_cb(prelude_option_t *option, const char *arg, prelude_string_t *err, void *context)
+{
+        normalize_to_ipv6 = TRUE;
+        return 0;
+}
+
+
+static int normalize_keep_ipv6(prelude_option_t *option, const char *arg, prelude_string_t *err, void *context)
+{
+        no_ipv6_prefix = FALSE;
+        return 0;
+}
+
+
 int normalize_LTX_manager_plugin_init(prelude_plugin_entry_t *pe, void *root_opt)
 {
+        prelude_option_t *opt;
         prelude_plugin_instance_t *pi;
         static manager_decode_plugin_t normalize;
+
+        setprotoent(1);
 
         memset(&normalize, 0, sizeof(normalize));
 
         prelude_plugin_set_name(&normalize, "Normalize");
         manager_decode_plugin_set_running_func(&normalize, normalize_run);
         prelude_plugin_entry_set_plugin(pe, (void *) &normalize);
+
+        prelude_option_add(root_opt, &opt, PRELUDE_OPTION_TYPE_CLI|PRELUDE_OPTION_TYPE_CFG,
+                           0, "normalize", "Option for the normalize plugin", PRELUDE_OPTION_ARGUMENT_NONE, NULL, NULL);
+
+        prelude_option_add(opt, NULL, PRELUDE_OPTION_TYPE_CLI|PRELUDE_OPTION_TYPE_CFG,
+                           '6', "ipv6-only", "Map IPv4 addresses to IPv6",
+                           PRELUDE_OPTION_ARGUMENT_NONE, normalize_to_ipv6_cb, NULL);
+
+        prelude_option_add(opt, NULL, PRELUDE_OPTION_TYPE_CLI|PRELUDE_OPTION_TYPE_CFG,
+                           '4', "keep-ipv4-mapped-ipv6",
+                           "Do not normalize IPv4 mapped IPv6 address to IPv4",
+                           PRELUDE_OPTION_ARGUMENT_NONE, normalize_keep_ipv6, NULL);
 
         return prelude_plugin_new_instance(&pi, (void *) &normalize, NULL, NULL);
 }
