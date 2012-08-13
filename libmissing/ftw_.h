1900a8092b24c2e6c418d0e78fbe78afe33c29db
Antoine LUONG <antoine.luong@c-s.fr>
2012-08-13 14:43:40 +0200

    Updated Free Software Foundation headers

--- libmissing/ftw_.h
+++ libmissing/ftw_.h
@@ -11,10 +11,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this file; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this file; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <sys/types.h>
