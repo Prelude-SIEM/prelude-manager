883d7ca7af5a84b365bca3e4809fcf8aaec155d6
Antoine LUONG <antoine.luong@c-s.fr>
2018-01-03 12:04:52 +0100

    [#2795] Copyright year update
    Change-Id: I918413558ea101f8308d0da8725432262635ed76

--- plugins/reports/smtp/smtp-io.c
+++ plugins/reports/smtp/smtp-io.c
@@ -1,6 +1,6 @@
 /*****
 *
-* Copyright (C) 2017 CS-SI. All Rights Reserved.
+* Copyright (C) 2017-2018 CS-SI. All Rights Reserved.
 * Author: Yoann Vandoorselaere <yoannv@gmail.com>
 *
 * This file is part of the Prelude-Manager program.
